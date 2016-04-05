affd1c0b72640e9ad4ca7b97dfb11d13f028a426
smallpawn <smallpawn@gmail.com>
2016-04-05 23:02:03 +0200

    Changed name and added view

---
+++ app/controllers/api/scrapper_types_controller.rb
@@ -0,0 +1,30 @@
+#scrapper_type_controller.rb
+
+require_relative 'base_controller'
+
+class Api::ScrapperTypesController < BaseController
+  def index
+    #respond_with Scrapper.all
+  end
+
+  def create
+    respond_with :api, :scrty, Scrapper.create(scrapper_params)
+  end
+
+  def destroy
+    respond_with Scrapper.destroy(params[:id])
+  end
+
+  def update
+    scrapper = Scrapper.find(params["id"])
+    scrapper.update_attributes(scrapper_params)
+    respond_with scrapper, json: scrapper
+  end
+
+  private
+
+  def scrapper_params
+    params.require(:scrapper).permit(:id, :category, :description)
+  end
+end
+
